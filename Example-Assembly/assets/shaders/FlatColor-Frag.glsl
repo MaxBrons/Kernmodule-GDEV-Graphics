66b1a1b58e24c4a5c7a32b904ba6130c09f41dd1
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2024-05-28 13:11:45 +0200

    Fixed flat color shader bug
    I used an openGL name as a variable, therefore it did not compile correctly. Changed the name and now it renders.

--- Example-Assembly/assets/shaders/FlatColor-Frag.glsl
+++ Example-Assembly/assets/shaders/FlatColor-Frag.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 
-layout(location = 0) out vec4 gl_Color;
+layout(location = 0) out vec4 a_Color;
 
 uniform vec3 u_Color;
 
 void main()
 {
-	gl_Color = vec4(u_Color, 1.0f);
+	a_Color = vec4(u_Color, 1.0f);
 }
