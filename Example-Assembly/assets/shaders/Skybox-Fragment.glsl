e00f1dcc4d1aea2fb6cb1e131ca871e34181b28f
MaxBrons <43110145+MaxBrons@users.noreply.github.com>
2024-06-06 16:44:23 +0200

    Added a sun to the skybox

--- Example-Assembly/assets/shaders/Skybox-Fragment.glsl
+++ Example-Assembly/assets/shaders/Skybox-Fragment.glsl
@@ -5,7 +5,7 @@ layout(location = 0) out vec4 FragColor;
 uniform vec3 u_LightDirection;
 uniform vec3 u_CameraPosition;
 
-in vec3 v_PixelCoords;
+in vec4 v_PixelCoords;
 
 vec3 lerp(vec3 a, vec3 b, float t){
 	return a + (b - a) * t;
@@ -16,7 +16,10 @@ void main()
 	vec3 topColor = vec3(68.0f / 255.0f, 118.0f / 255.0f, 189.0f / 255.0f);
 	vec3 botColor = vec3(188.0f / 255.0f, 214.0f / 255.0f, 231.0f / 255.0f);
 
-	vec3 viewDir = normalize(v_PixelCoords - u_CameraPosition);
+	vec3 sunColor = vec3(1.0f, 200.0f / 255.0f, 50.0f / 255.0f);
+
+	vec3 viewDir = normalize(v_PixelCoords.rgb - u_CameraPosition);
+	float sun = pow(min(max(dot(viewDir, u_LightDirection), 0.0f), 1.0f), 128);
 	
-	FragColor = vec4(lerp(botColor, topColor, max(viewDir.y, 0.0)), 1);
+	FragColor = vec4(lerp(botColor, topColor, abs(viewDir.y)) + sun * sunColor, 1);
 }
--- Example-Assembly/assets/shaders/Skybox-Vertex.glsl
+++ Example-Assembly/assets/shaders/Skybox-Vertex.glsl
@@ -5,11 +5,11 @@ layout(location = 3) in vec3 a_Normal;
 uniform mat4 u_ViewProjection;
 uniform mat4 u_Transform;
 
-out vec3 v_PixelCoords;
+out vec4 v_PixelCoords;
 
 void main()
 {
-	v_PixelCoords = mat3(u_Transform) * a_Position;
+	v_PixelCoords = u_Transform * vec4(a_Position, 1.0);
 
-	gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
+	gl_Position = u_ViewProjection * v_PixelCoords;
 }
